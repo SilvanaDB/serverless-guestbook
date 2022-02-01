9e5bcc1b46526b35bf776fc85b4604824a7b242e
SilvanaDB <33125232+SilvanaDB@users.noreply.github.com>
2022-02-01 13:12:28 +0100

    update

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://2c66def5.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
